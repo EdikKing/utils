7a48959b8fad4c39701d92aa65905f7191bd8e9c
Edik <wfj@edik.cn>
2019-01-18 13:46:05 +0800

    first commit

---
+++ src/main/java/cn/edik/EdikKit.java
@@ -0,0 +1,12 @@
+package cn.edik;
+
+/**
+ * Author    : edik
+ * CreateTime: 2019-01-18 13:41
+ **/
+public class EdikKit {
+
+    public static String getHello(){
+        return "Hello Edik";
+    }
+}
